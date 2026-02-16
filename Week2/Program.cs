a6fa49a057a568ec9f439f3e53fca39ba4a24556
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-02-16 22:32:01 +0000

    added unit examples

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Unit marine = new Unit() {Name = "Marine",Health = 45, Damage = 6 };
+Unit zergling = new Unit() { Name = "Zergling", Health = 35, Damage = 5 };
+Unit ultralisk = new Unit() { Name = "Ultralisk", Health = 500, Damage = 35 };
+
+Console.WriteLine("Stats of the units using \"PrintStats\" Method:");
+marine.PrintStats();
+zergling.PrintStats();
+ultralisk.PrintStats();
+
+Console.WriteLine("\nStats of the units using \"ToString\" Method:");
+// To string is automatically called when we try to print an object as a string.
+Console.WriteLine(marine);
+Console.WriteLine(zergling);
+Console.WriteLine(ultralisk);
+
+marine.Health = 200;
+
+Console.WriteLine($"\nStats of the units after changing the health of the marine: {marine}");
+
