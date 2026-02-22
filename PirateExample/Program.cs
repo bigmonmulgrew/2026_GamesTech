acef16bc3e56ee5cf283aaec3ee7d0f72905f9ef
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-02-22 13:51:42 +0000

    added simplified unit case.

---
+++ PirateExample/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// It is not uncommon to see a variable share the name of the class,
+// but it can lead to confusion.
+// There are some cases where it is an extremely common usage, but in most cases, it is better to avoid it.
+Unit unit = new Unit("Marine", 20, 5);
+
+
---
+++ PirateExample/Unit.cs
@@ -0,0 +1,22 @@
+﻿class Unit
+{
+    private string unitType;
+    private int health;
+    private float damage;
+
+    // Data that belongs to ONE unit
+    public string UnitType { get { return unitType; } set { unitType = value; } }
+    public float Health { get { return health; }  set { health = (int)value; } }
+    public float Damage { get { return damage; } set { damage = value; } }
+    public Unit(string unitType, int health, float damage)
+    {
+        this.unitType = unitType;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public void PrintStats() { Console.WriteLine($"Name: {UnitType}, Health: {Health}, Damage: {Damage}"); }
+
+   
+    public override string ToString() { return $"Name: {UnitType}, Health: {Health}, Damage: {Damage}"; }
+}
