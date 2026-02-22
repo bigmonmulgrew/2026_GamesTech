4e1fb4caafb76ac858bf50f2fb8170f2576d1d6d
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-02-22 23:09:57 +0000

    add collections

--- PirateExample/Pirate.cs
+++ PirateExample/Pirate.cs
@@ -1,6 +1,12 @@
-﻿public class Pirate : Unit
+﻿using System.Collections.Generic; // We need this, but do we actually need to type it, and why?
+
+public class Pirate : Unit
 {
     string pirateShip;
+    List<Pirate> crew = new();
+    List<Unit> prisoners = new();
+    Queue<string> destinations = new();
+
     bool hasEyePatch;   
     bool hasPegLeg;
     bool hasHookHand;
@@ -8,6 +14,20 @@
     bool isCaptain;
 
     string treasureClue;
+
+    // Collapse to keep things tidy
+    #region Properties  
+    public string PirateShip        { get { return pirateShip; } set { pirateShip = value; } }
+    public List<Pirate> Crew        { get { return crew; } set { crew = value; } }
+    public List<Unit> Prisoners     { get { return prisoners; } set { prisoners = value; } }
+    public Queue<string> Destinations { get { return destinations; } set { destinations = value; } }
+    public bool HasEyePatch         { get { return hasEyePatch; } set { hasEyePatch = value; } }
+    public bool HasPegLeg           { get { return hasPegLeg; } set { hasPegLeg = value; } }
+    public bool HasHookHand         { get { return hasHookHand; } set { hasHookHand = value; } }
+    public bool HasParrot           { get { return hasParrot; } set { hasParrot = value; } }
+    public bool IsCaptain           { get { return isCaptain; } set { isCaptain = value; } }
+    public string TreasureClue      { get { return treasureClue; } set { treasureClue = value; } }
+    #endregion
 
     public Pirate(string unitType, int health, float damage, string pirateShip) 
         : base(unitType, health, damage)
--- PirateExample/Program.cs
+++ PirateExample/Program.cs
@@ -1,11 +1,19 @@
 ﻿
-// It is not uncommon to see a variable share the name of the class,
-// but it can lead to confusion.
-// There are some cases where it is an extremely common usage, but in most cases, it is better to avoid it.
+Pirate captain = new Pirate("Blackbeard", 150, 50, "Sea++");    
 
+Pirate crew1 = new Pirate("Will", 90, 25, "Sea++");  // We are repeating data, consider using a constant
+Pirate crew2 = new Pirate("Anne", 95, 30, "Sea++");
 
-Unit unit = new Unit("Marine", 20, 5);
-Pirate pirate = new Pirate("Blackbeard", 100, 20, "Grey Pearl");
+// Add the crew members to the captain's crew list
+captain.Crew.Add(crew1);
+captain.Crew.Add(crew2);
 
-Console.WriteLine(pirate);
-// Output: Arr! I be Blackbeard of the Grey Pearl!
+// Take a prisoner and add them to the captain's prisoners list
+captain.Prisoners.Add(new Unit("Captured Soldier", 50, 5));
+
+// Add some destinations to the captain's destinations queue
+captain.Destinations.Enqueue("Tortuga");
+captain.Destinations.Enqueue("Skull Island");
+
+Console.WriteLine("Next destination: " + captain.Destinations.Dequeue());
+// Output: Next destination: Tortuga
