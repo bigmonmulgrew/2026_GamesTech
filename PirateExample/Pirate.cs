ab39e9c2c4032f745990f73557cbceacd6e6f18e
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-02-23 13:29:00 +0000

    added a type casting example

--- PirateExample/Pirate.cs
+++ PirateExample/Pirate.cs
@@ -45,4 +45,61 @@ public class Pirate : Unit
     {
             return $"Arr! I be {unitType} of the {pirateShip}!";
     }
+    /// <summary>
+    /// Deals with all the prisoners. No data needed, the Pirate should already have it
+    /// </summary>
+    public void ProcessPrisoners()      // note this is plural.
+    {
+        // Only the pirate captain should be able to process prisoners
+        // Or mauybe we could implment a first mate too?
+
+        if (!isCaptain) 
+        {
+            Console.WriteLine($"{unitType} is not the captain and cannot process prisoners.");
+            return; 
+        }
+
+        // Next we need to check if we even have prisoners.
+        if (prisoners.Count == 0)
+        {
+            Console.WriteLine($"There are currently no prisoners on the {pirateShip.Name}");
+            return;
+        }
+
+        // Now we start to actually process the prisoners
+
+        foreach (Unit prisoner in prisoners)
+        {
+            // Instead of nesting this, lets make a function that focusses on what we need to do to process one prisoner.
+            // This will repeat that for each member
+            ProcessPrisoner(prisoner);
+        }
+    }
+
+    // Note this is singular, and needs to be passed a prisoner. It is also private
+    // It only exists to abstract functionality from ProcessPrisoners()
+    private void ProcessPrisoner(Unit prisoner) 
+    {
+        // Check if prisoner is null incase of improper usage
+        if (prisoner == null) return;
+
+        // Now we need to know what type of prisoner we are dealing with, pirate or civilian.
+        // All units that are not pirate are assumed to be a civilian for our purposes.
+
+        // The prisoner is a unit but we need to check it is also a prisoner
+        // Thetre are many ways to do this
+
+        Pirate pirate = prisoner as Pirate; 
+
+        if (pirate != null)
+        {
+            Console.WriteLine($"{pirate.UnitType} will walk the plank tonight! His {prisoner.Health * prisoner.Damage} gold will be added to our treasure chest");
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"A randsom note for {prisoner.Health * prisoner.Damage} gold has beed dispatched to {prisoner.UnitType}'s family.");
+        }
+
+    }
 }
--- PirateExample/Program.cs
+++ PirateExample/Program.cs
@@ -8,9 +8,11 @@ if (FileManager.LoadText(FileManager.LogFilePath) != null)
 }
 
 
-Ship ship = new Ship("Sea++", 100, new Vector2(0, 0));
+Ship ship = new Ship("Ocean's Unity", 100, new Vector2(5, 4));
+Ship rivalShip = new Ship("UoD Ureal", 250, new Vector2(4, 5));
 
 Pirate captain = new Pirate("Blackbeard", 150, 50, ship);    
+captain.IsCaptain = true;
 
 Pirate crew1 = new Pirate("Will", 90, 25, ship); 
 Pirate crew2 = new Pirate("Anne", 95, 30, ship);
@@ -20,7 +22,12 @@ captain.Crew.Add(crew1);
 captain.Crew.Add(crew2);
 
 // Take a prisoner and add them to the captain's prisoners list
-captain.Prisoners.Add(new Unit("Captured Soldier", 50, 5));
+captain.Prisoners.Add(new Unit("Dave", 50, 5));
+captain.Prisoners.Add(new Unit("Jon", 50, 5));
+captain.Prisoners.Add(new Pirate("Jeremy", 80, 50, rivalShip));
+captain.Prisoners.Add(new Unit("Hayden", 50, 5));
+captain.Prisoners.Add(new Unit("Isaac", 50, 5));
+captain.Prisoners.Add(new Pirate("Patrick", 80, 15, rivalShip));
 
 // Add some destinations to the captain's destinations queue
 captain.Destinations.Enqueue("Tortuga");
@@ -38,3 +45,12 @@ Console.WriteLine(Directory.GetCurrentDirectory());
 FileManager.SaveText($"Captain's log: Set sail for adventure!\n" +
     $"Treasure clue: {captain.TreasureClue}");
 Console.WriteLine($"Log saved to: {FileManager.LogFilePath}");
+
+Console.Write("\n\n\n"); // Just some empty lines to create some space.
+
+Console.WriteLine($"Crew 1 processing prisoners");
+crew1.ProcessPrisoners();
+
+Console.Write("\n\n\n"); // Just some empty lines to create some space.
+Console.WriteLine($"Captain processing prisoners");
+captain.ProcessPrisoners();
