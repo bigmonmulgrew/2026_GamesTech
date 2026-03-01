f3215bde44336b7611e8dedfb7e3b5e669ac3746
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-03-01 21:45:18 +0000

    Hello Dave

--- HelloWorld/Assets/HelloWorld.cs
+++ HelloWorld/Assets/HelloWorld.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    string userName = "Dave";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, World!");
+        Debug.Log($"Hello, {userName}!");
     }
 
     // Update is called once per frame
