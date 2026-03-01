e40be2b384a4d5f9f0b54654d2997aefd6d36efe
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-03-01 21:54:36 +0000

    Add serialized field

--- HelloWorld/Assets/HelloWorld.cs
+++ HelloWorld/Assets/HelloWorld.cs
@@ -1,19 +1,12 @@
-using System;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    string userName = "Dave";
+    [SerializeField] string userName = "Dave";
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log($"Hello, {userName}!");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
