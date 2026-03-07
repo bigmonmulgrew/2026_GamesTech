3bf246aa4b3a53b90cb5808ce1e9bbc25bb9ef8a
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-03-07 20:27:16 +0000

    setup all inputs on game manager

--- CYOA/ChooseYouOwnAdventure/Assets/Scripts/GameManager.cs
+++ CYOA/ChooseYouOwnAdventure/Assets/Scripts/GameManager.cs
@@ -1,7 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] InputAction inputAction1;
+    [SerializeField] InputAction inputAction2;
+
+    void OnEnable()
+    {
+        inputAction1.Enable();
+        inputAction2.Enable();
+    }
+    void OnDisable()
+    {
+        inputAction1.Disable();
+        inputAction2.Disable();
+    }
+    private void Update()
+    {
+        if (inputAction1.WasPressedThisFrame())
+        {
+            Debug.Log($"Input 1 pressed the button!");
+        }
+        if (inputAction2.WasPressedThisFrame())
+        {
+            Debug.Log($"Input 2 pressed the button!");
+        }
+    }
+
     public void Button1Click()
     {
         Debug.Log("Button 1 clicked!");
