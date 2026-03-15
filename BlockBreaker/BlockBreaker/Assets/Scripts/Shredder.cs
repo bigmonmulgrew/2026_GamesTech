0ddfa811c794a4cff99a8982cf27a1e6439c2e7d
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-03-15 23:29:20 +0000

    Add walls and shredder

--- BlockBreaker/BlockBreaker/Assets/Scripts/Shredder.cs
+++ BlockBreaker/BlockBreaker/Assets/Scripts/Shredder.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class Shredder : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(collision.gameObject);
     }
 }
