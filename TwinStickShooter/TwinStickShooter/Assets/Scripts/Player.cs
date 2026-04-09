7d0c20c53dfe2f37f303cbfcd9bf0bd02b111ede
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-04-09 21:40:07 +0100

    added player movement

--- TwinStickShooter/TwinStickShooter/Assets/Scripts/Player.cs
+++ TwinStickShooter/TwinStickShooter/Assets/Scripts/Player.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float moveSpeed = 5f;
+
+    DefaultInputActions playerInputs;
+    InputAction moveAction;
+
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        playerInputs = new DefaultInputActions();
+        moveAction = playerInputs.Player.Move;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {        
+       playerInputs.Player.Enable();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + moveAction.ReadValue<Vector2>() * moveSpeed * Time.deltaTime);
     }
 }
