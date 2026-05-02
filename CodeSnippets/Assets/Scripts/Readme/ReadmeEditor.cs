4081a257ca0422b1b233c23645f0e35a45406a43
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-05-02 18:31:48 +0100

    Added readme script

---
+++ CodeSnippets/Assets/Scripts/Readme/ReadmeEditor.cs
@@ -0,0 +1,90 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomEditor(typeof(Readme))]
+public class ReadmeEditor : Editor
+{
+    private bool isEditing;
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        SerializedProperty textProperty = serializedObject.FindProperty("text");
+
+        EditorGUILayout.Space();
+
+        if (isEditing)
+        {
+            EditorGUILayout.LabelField("Readme", EditorStyles.boldLabel);
+
+            textProperty.stringValue = EditorGUILayout.TextArea(
+                textProperty.stringValue,
+                GUILayout.MinHeight(120)
+            );
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Save"))
+            {
+                isEditing = false;
+                GUI.FocusControl(null);
+            }
+        }
+        else
+        {
+            DrawFormattedReadme(textProperty.stringValue);
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Edit"))
+            {
+                isEditing = true;
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawFormattedReadme(string text)
+    {
+        GUIStyle style = new GUIStyle(EditorStyles.helpBox)
+        {
+            richText = true,
+            wordWrap = true,
+            padding = new RectOffset(10, 10, 8, 8)
+        };
+
+        EditorGUILayout.LabelField(text, style);
+    }
+}
+
+public static class ReadmeEditorUtility
+{
+    public static void MoveReadmeBelowTransform(Readme readme)
+    {
+        if (readme == null)
+        {
+            return;
+        }
+
+        EditorApplication.delayCall += () =>
+        {
+            if (readme == null)
+            {
+                return;
+            }
+
+            while (ComponentUtility.MoveComponentUp(readme))
+            {
+                // Moves as high as Unity permits.
+                // Transform will remain above it.
+            }
+        };
+    }
+}
+
+#endif
